1d99cc92b34fcd8cbc41caf3393d8836559d3ad9
Lex Harie Pisco <lexthegreat223@gmail.com>
2025-12-09 01:29:15 +0800

    Add users service

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, passwordHash: string, name?: string) {
+    const user = this.usersRepository.create({ email, passwordHash, name });
+    return this.usersRepository.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: string) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+}
