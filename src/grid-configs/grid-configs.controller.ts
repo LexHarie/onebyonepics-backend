e0f12b97673506e8d057eb0d519e211aab85b512
Lex Harie Pisco <lexthegreat223@gmail.com>
2025-12-09 01:39:31 +0800

    Add grid configurations controller

---
+++ src/grid-configs/grid-configs.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { GridConfigsService } from './grid-configs.service';
+
+@Controller('grid-configs')
+export class GridConfigsController {
+  constructor(private readonly gridConfigsService: GridConfigsService) {}
+
+  @Get()
+  findAll() {
+    return this.gridConfigsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.gridConfigsService.findById(id);
+  }
+}
