96874ea0409dbc4361e7c90d2509b7d8184ac655
Lex Harie Pisco <lexthegreat223@gmail.com>
2025-12-18 00:32:50 +0800

    Update generation module

--- src/generation/generation.module.ts
+++ src/generation/generation.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GenerationController } from './generation.controller';
 import { GenerationService } from './generation.service';
+import { GenerationProcessor } from './generation.processor';
 import { ImagesModule } from '../images/images.module';
 import { StorageModule } from '../storage/storage.module';
 import { GenAIModule } from '../genai/genai.module';
 import { AuthModule } from '../auth/auth.module';
+import { QuotasModule } from '../quotas/quotas.module';
+import { WatermarkModule } from '../watermark/watermark.module';
+import { QueueModule } from '../queue/queue.module';
 
 @Module({
   imports: [
@@ -14,8 +18,12 @@ import { AuthModule } from '../auth/auth.module';
     StorageModule,
     GenAIModule,
     AuthModule,
+    QuotasModule,
+    WatermarkModule,
+    QueueModule,
   ],
   controllers: [GenerationController],
-  providers: [GenerationService],
+  providers: [GenerationService, GenerationProcessor],
+  exports: [GenerationService],
 })
 export class GenerationModule {}
