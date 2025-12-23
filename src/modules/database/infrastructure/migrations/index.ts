fd587b833e55fb305a9521d869c5b8fbf418899e
Lex Harie Pisco <lexthegreat223@gmail.com>
2025-12-24 02:31:53 +0800

    Add order item migration to migration index

--- src/modules/database/infrastructure/migrations/index.ts
+++ src/modules/database/infrastructure/migrations/index.ts
@@ -9,6 +9,7 @@ import { migration007AddWebhookEvents } from './007_add_webhook_events';
 import { migration008AddWebhookVerification } from './008_add_webhook_verification';
 import { migration009AddAdminRole } from './009_add_admin_role';
 import { migration010AddDailyStats } from './010_add_daily_stats';
+import { migration011AddOrderItems } from './011_add_order_items';
 
 // Export all migrations in order
 // Add new migrations to this array as they are created
@@ -23,6 +24,7 @@ export const migrations: Migration[] = [
   migration008AddWebhookVerification,
   migration009AddAdminRole,
   migration010AddDailyStats,
+  migration011AddOrderItems,
 ];
 
 export type { Migration } from './migration.interface';
